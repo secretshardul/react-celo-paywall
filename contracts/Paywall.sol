ee9d3f0716e4568d3ae8d32713723df814122735
shardul <shardul.aeer@somaiya.edu>
2021-04-14 16:05:22 +0530

    feat: Contract to store and query article buyers

--- contracts/Paywall.sol
+++ contracts/Paywall.sol
@@ -2,5 +2,27 @@
 pragma solidity ^0.8;
 
 contract Paywall {
-    mapping(address => uint) public balances;
+    event NewPurchase(address user, string articleId);
+
+    // Map users to articles
+    mapping(address => string[]) public userPurchases;
+    // Map articles to users
+    mapping(string => address[]) public articlePurchasers;
+
+    function purchaseAccess(string calldata articleId) public payable {
+        // require(msg.value == 0.00001 ether);
+
+        userPurchases[msg.sender].push(articleId);
+        articlePurchasers[articleId].push(msg.sender);
+
+        emit NewPurchase(msg.sender, articleId);
+    }
+
+    function getUserPurchaseCount(address userAddress) public view returns (uint) {
+        return userPurchases[userAddress].length;
+    }
+
+     function getArticlePurchaserCount(string calldata articleId) public view returns (uint) {
+        return articlePurchasers[articleId].length;
+    }
 }
